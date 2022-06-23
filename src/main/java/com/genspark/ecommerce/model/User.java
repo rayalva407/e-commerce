7213487a8ed1620922899da81bb63a43b4a4e2ae
rayalva407 <rayalva407@gmail.com>
2022-06-23 16:12:39 -0600

    Creates JPA Entities

---
+++ src/main/java/com/genspark/ecommerce/model/User.java
@@ -0,0 +1,23 @@
+package com.genspark.ecommerce.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable
+    private Set<Role> roles;
+
+}
